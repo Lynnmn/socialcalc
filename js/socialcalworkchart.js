e313aeccd20d84b0636b9919fe3797eb0862b84a
SmartXiaoMing <95813422@qq.com>
2018-12-01 10:22:44 +0800

    add chart bar and area
    N/A
    
    add chart bar and area
    Signed-off-by: SmartXiaoMing <95813422@qq.com>

--- js/socialcalworkchart.js
+++ js/socialcalworkchart.js
@@ -81,146 +81,9 @@ $(document).ready(function() {
         } else if(selectedChart === 'line') {
             return renderChartLine({title:"haha", subtitle:"sub haha", firstColAsX:true, firstRowAsS:true});
         } else if(selectedChart === 'bar') {
-            var chart = {
-                type: 'column'
-             };
-             var title = {
-                text: '柱状图'   
-             };
-             var subtitle = {
-                text: 'Source: runoob.com'  
-             };
-             var xAxis = {
-                categories: ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'],
-                crosshair: true
-             };
-             var yAxis = {
-                min: 0,
-                title: {
-                   text: 'Rainfall (mm)'         
-                }      
-             };
-             var tooltip = {
-                headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
-                pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
-                   '<td style="padding:0"><b>{point.y:.1f} mm</b></td></tr>',
-                footerFormat: '</table>',
-                shared: true,
-                useHTML: true
-             };
-             var plotOptions = {
-                column: {
-                   pointPadding: 0.2,
-                   borderWidth: 0
-                }
-             };  
-             var credits = {
-                enabled: false
-             };
-             
-             var series= [{
-                  name: 'Tokyo',
-                      data: [49.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
-                  }, {
-                      name: 'New York',
-                      data: [83.6, 78.8, 98.5, 93.4, 106.0, 84.5, 105.0, 104.3, 91.2, 83.5, 106.6, 92.3]
-                  }, {
-                      name: 'London',
-                      data: [48.9, 38.8, 39.3, 41.4, 47.0, 48.3, 59.0, 59.6, 52.4, 65.2, 59.3, 51.2]
-                  }, {
-                      name: 'Berlin',
-                      data: [42.4, 33.2, 34.5, 39.7, 52.6, 75.5, 57.4, 60.4, 47.6, 39.1, 46.8, 51.1]
-             }];     
-                
-            //  var json = {};   
-             json.chart = chart; 
-             json.title = title;   
-             json.subtitle = subtitle; 
-             json.tooltip = tooltip;
-             json.xAxis = xAxis;
-             json.yAxis = yAxis;  
-             json.series = series;
-             json.plotOptions = plotOptions;  
-             json.credits = credits;
-            //  $('#chart-container').highcharts(json);
+            return renderChartBar({title:"haha", subtitle:"sub haha", firstColAsX:true, firstRowAsS:true});
         } else if(selectedChart === 'area') {
-            var chart = {
-                type: 'area'
-             };
-             var title = {
-                text: 'US and USSR nuclear stockpiles'   
-             };
-             var subtitle = {
-                text: 'Source: <a href="http://thebulletin.metapress.com/content/c4120650912x74k7/fulltext.pdf">' +
-                   'thebulletin.metapress.com</a>'  
-             };
-             var xAxis = {
-                allowDecimals: false,
-                labels: {
-                   formatter: function () {
-                      return this.value; // clean, unformatted number for year
-                   }
-                }
-             };
-             var yAxis = {
-                title: {
-                   text: 'Nuclear weapon states'
-                },
-                labels: {
-                   formatter: function () {
-                      return this.value / 1000 + 'k';
-                   }
-                }
-             };
-             var tooltip = {
-                pointFormat: '{series.name} produced <b>{point.y:,.0f}</b><br/>warheads in {point.x}'
-             };
-             var plotOptions = {
-                area: {
-                   pointStart: 1940,
-                   marker: {
-                      enabled: false,
-                      symbol: 'circle',
-                      radius: 2,
-                      states: {
-                         hover: {
-                           enabled: true
-                         }
-                      }
-                   }
-                }
-             };
-             var series= [{
-                   name: 'USA',
-                      data: [null, null, null, null, null, 6, 11, 32, 110, 235, 369, 640,
-                          1005, 1436, 2063, 3057, 4618, 6444, 9822, 15468, 20434, 24126,
-                          27387, 29459, 31056, 31982, 32040, 31233, 29224, 27342, 26662,
-                          26956, 27912, 28999, 28965, 27826, 25579, 25722, 24826, 24605,
-                          24304, 23464, 23708, 24099, 24357, 24237, 24401, 24344, 23586,
-                          22380, 21004, 17287, 14747, 13076, 12555, 12144, 11009, 10950,
-                          10871, 10824, 10577, 10527, 10475, 10421, 10358, 10295, 10104]
-                  }, {
-                      name: 'USSR/Russia',
-                      data: [null, null, null, null, null, null, null, null, null, null,
-                          5, 25, 50, 120, 150, 200, 426, 660, 869, 1060, 1605, 2471, 3322,
-                          4238, 5221, 6129, 7089, 8339, 9399, 10538, 11643, 13092, 14478,
-                          15915, 17385, 19055, 21205, 23044, 25393, 27935, 30062, 32049,
-                          33952, 35804, 37431, 39197, 45000, 43000, 41000, 39000, 37000,
-                          35000, 33000, 31000, 29000, 27000, 25000, 24000, 23000, 22000,
-                          21000, 20000, 19000, 18000, 18000, 17000, 16000]
-                }
-             ];     
-                
-            //  var json = {};   
-             json.chart = chart; 
-             json.title = title;   
-             json.subtitle = subtitle; 
-             json.tooltip = tooltip;
-             json.xAxis = xAxis;
-             json.yAxis = yAxis;  
-             json.series = series;
-             json.plotOptions = plotOptions;
-            //  $('#chart-container').highcharts(json);
+            return renderChartBar({title:"haha", subtitle:"sub haha", firstColAsX:true, firstRowAsS:true});
         }
         return json
     }
@@ -318,6 +181,10 @@ $(document).ready(function() {
             borderWidth: 0
         };
 
+        var credits = {
+            enabled: false
+        };
+
         var json = {};
         json.title = {text: config.title };
         json.subtitle = {text: config.subtitle};
@@ -326,6 +193,186 @@ $(document).ready(function() {
         json.tooltip = tooltip;
         json.legend = legend;
         json.series = series;
+        json.credits = credits;
+        return json;
+    }
+
+    function renderChartBar(config) {
+        // config:{title, subtitle, tooltip, legend, firstColAsX, firstRowAsS}
+        const editor = SocialCalc.Keyboard.focusTable
+        const cells = workbook.getCurrentSheet().cells
+        const { anchorcoord, bottom, top, left, right } = editor.range || {}
+        let range = editor.range;
+        let startCol = config.firstColAsX ? range.left + 1 : range.left;
+        let startRow = config.firstRowAsS ? range.top + 1 : range.top;
+        var xAxis = {
+            categories: []
+        };
+        for (let row = startRow; row<=range.bottom; row++) {
+            if (config.firstColAsX) {
+                xAxis.categories.push(cells[SocialCalc.crToCoord(range.left, row)].datavalue);
+            } else {
+                xAxis.categories.push("");
+            }
+        }
+        var series =  [];
+        if (config.firstRowAsS) {
+            for (let col = startCol; col <= range.right; col++) {
+                let s = {
+                    name: config.firstRowAsS ? cells[SocialCalc.crToCoord(col, range.top)].datavalue : "",
+                    data: []
+                };
+                series.push(s);
+            }
+        }
+
+        for (let col = startCol; col <= range.right; col++) {
+            let data = series[col - startCol].data;
+            for (let row=startRow; row<=range.bottom; row++) {
+                let coord = SocialCalc.crToCoord(col, row);
+                var value = cells[coord].datavalue;
+                data.push(value);
+            }
+        }
+
+        var yAxis = {
+            title: {
+               text: ''
+            },
+            plotLines: [{
+               value: 0,
+               width: 1,
+               color: '#808080'
+            }]
+        };
+
+        var tooltip = {
+            valueSuffix: ''
+        }
+
+        var legend = {
+            layout: 'vertical',
+            align: 'right',
+            verticalAlign: 'middle',
+            borderWidth: 0
+        };
+
+        var credits = {
+            enabled: false
+        };
+
+        var plotOptions = {
+            column: {
+               pointPadding: 0.2,
+               borderWidth: 0
+            }
+         };
+
+        var json = {};
+        json.chart = {type: 'column'};
+        json.title = {text: config.title };
+        json.subtitle = {text: config.subtitle};
+        json.xAxis = xAxis;
+        json.yAxis = yAxis;
+        json.tooltip = tooltip;
+        json.legend = legend;
+        json.series = series;
+        json.plotOptions = plotOptions;
+        json.credits = credits;
+        return json;
+    }
+
+    function renderChartBar(config) {
+    // config:{title, subtitle, tooltip, legend, firstColAsX, firstRowAsS}
+        const editor = SocialCalc.Keyboard.focusTable
+        const cells = workbook.getCurrentSheet().cells
+        const { anchorcoord, bottom, top, left, right } = editor.range || {}
+        let range = editor.range;
+        let startCol = config.firstColAsX ? range.left + 1 : range.left;
+        let startRow = config.firstRowAsS ? range.top + 1 : range.top;
+        var xAxis = {
+            categories: []
+        };
+        for (let row = startRow; row<=range.bottom; row++) {
+            if (config.firstColAsX) {
+                xAxis.categories.push(cells[SocialCalc.crToCoord(range.left, row)].datavalue);
+            } else {
+                xAxis.categories.push("");
+            }
+        }
+        var series =  [];
+        if (config.firstRowAsS) {
+            for (let col = startCol; col <= range.right; col++) {
+                let s = {
+                    name: config.firstRowAsS ? cells[SocialCalc.crToCoord(col, range.top)].datavalue : "",
+                    data: []
+                };
+                series.push(s);
+            }
+        }
+
+        for (let col = startCol; col <= range.right; col++) {
+            let data = series[col - startCol].data;
+            for (let row=startRow; row<=range.bottom; row++) {
+                let coord = SocialCalc.crToCoord(col, row);
+                var value = cells[coord].datavalue;
+                data.push(value);
+            }
+        }
+
+        var yAxis = {
+            title: {
+               text: 'Nuclear weapon states'
+            },
+            labels: {
+               formatter: function () {
+                  return this.value / 1000 + 'k';
+               }
+            }
+         };
+
+        var tooltip = {
+            pointFormat: '{series.name} produced <b>{point.y:,.0f}</b><br/>warheads in {point.x}'
+        };
+
+        var legend = {
+            layout: 'vertical',
+            align: 'right',
+            verticalAlign: 'middle',
+            borderWidth: 0
+        };
+
+        var credits = {
+            enabled: false
+        };
+
+       var plotOptions = {
+           area: {
+              pointStart: 1940,
+              marker: {
+                 enabled: false,
+                 symbol: 'circle',
+                 radius: 2,
+                 states: {
+                    hover: {
+                      enabled: true
+                    }
+                 }
+              }
+           }
+       };
+
+        var json = {};
+        json.chart = {type: 'area'};
+        json.title = {text: config.title };
+        json.subtitle = {text: config.subtitle};
+        json.xAxis = xAxis;
+        json.yAxis = yAxis;
+        json.tooltip = tooltip;
+        json.legend = legend;
+        json.series = series;
+        json.plotOptions = plotOptions;
+        json.credits = credits;
         return json;
     }
 })
